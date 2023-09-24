4de8ade8605140903df83eb67c87414f4863a915
Виталий Иванов <vitalkaivanov009@mail.ru>
2023-09-24 19:57:00 +0300

    занятие 13 Drag and Drop

---
+++ DragAndDrop/DragAndDrop/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DragAndDrop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label lbl = (Label)sender;
+            DragDrop.DoDragDrop(lbl, lbl.Content, DragDropEffects.Copy);
+        }
+
+        private void txtTarget_Drop(object sender, DragEventArgs e)
+        {
+            ((TextBlock)sender).Text = (string)e.Data.GetData(DataFormats.Text);
+        }
+    }
+}
